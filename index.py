4bac2f441b662e1eb3acb803390bc168ae110a27
Brandon Colangelo <brandoncancode@gmail.com>
2023-05-12 09:27:08 -0400

    Initial Commit

---
+++ index.py
@@ -0,0 +1,9 @@
+# Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+
+def less_than_or_equal(num):
+    if num <= 0:
+        return True
+    else:
+        return False
+    
+print(less_than_or_equal(1))
